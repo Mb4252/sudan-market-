7b72291819eb198e540f555c376be72b82bef36d
Mb4252 <118615883+Mb4252@users.noreply.github.com>
2026-04-06 20:52:40 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-اconst mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const crypto = require('crypto');
 const { ethers } = require('ethers');
 const { Connection, Keypair, LAMPORTS_PER_SOL } = require('@solana/web3.js');
